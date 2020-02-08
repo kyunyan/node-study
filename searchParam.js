29a7d4da52a6422a180f1fbd1bb4f94ec081b331
geonwan <psog03081@naver.com>
2020-02-08 22:14:45 +0900

    searchParam모듈 및 보안 crypto

--- searchParam.js
+++ searchParam.js
@@ -9,6 +9,7 @@
  // front 에서도  url 객체와 searchParam객체가 있는데 그럴때 많이 사용한다.
 // 도메인 부분은 같은 도메인일때 생략할수 있는데 그럴때는 whatw방식은 안되서 url.parse방식을 사용한다
 // 나머지는 이렇게 사용한다.
+
  console.log("searchParams.getAll() ", myURL.searchParams.getAll("category"));          // 카테고리에 node 및 자바스크립트가 나온다
  console.log("searchParams.get()", myURL.searchParams.get("limit"));                    // 이거 하나만 가자여노다 10
  console.log("searchParams.has()" , myURL.searchParams.has("page"));                    // page 값을 있으면 true
