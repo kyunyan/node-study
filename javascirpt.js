4a63db2deed0871a9365f5436caf3c81a0673517
leegeonwan <gwlee@exciform.com>
2020-09-28 17:24:21 +0900

    test

---
+++ javascirpt.js
@@ -0,0 +1,36 @@
+/**
+   크롬 브라우저에 대해서 설명
+   console.log();
+   코드 센드박스.
+   
+   변수 상수
+
+   특정이름에 특정 값을 담는 것을 선언
+   
+    const       상수        2 선언 불가능       블록 스코프         es6
+    let         변수        2선언 불가능        블록 스코프         es6
+    var         변수        계속 선언가능       함수형 스코프       es5
+
+    null undefined 둘다 없다 라는것을 의미한다.
+
+    null 은 진짜 없다
+    undefined 정의 되지 않았다.
+
+
+    연산자 = 대입연산자
+    산술연산자
+
+    논리연산자
+    // Not !             -> valdation 체크 !
+    // AND &&            
+    // OR ||             -> var footerColor = options.footerColor || "#008000"     
+
+    비교연산자
+    const a = 1;
+    const b = 1;
+    // false == 0 ;  true == 1;
+    conse.log(a == b);          // 타입 비교 x
+    conse.log(a === b);         // 타입
+
+    
+*/
