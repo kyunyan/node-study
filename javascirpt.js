5272a434981db6603dbeec3b6132a03b7590c1d6
geonwan <psog03081@naver.com>
2020-11-10 01:06:02 +0900

    test

--- javascirpt.js
+++ javascirpt.js
@@ -117,6 +117,9 @@
     const f3 = (a,b) => a+b;
 
     call apply, bind
+    call 메서드는 모든 함수에서 사용할 수 있으며 this를 특정 값으로 지정할수 있다.
+    함수를 호출하면서 call을 사용하고 this를 사용할 객체를 넘기면 해당 함수의 주어진 객체의 메서드 인것처럼 사용
+
     const bruce = {name : "bruce"};
 
     function greet(){
@@ -137,6 +140,30 @@
     
     bruce
     {name: "bruce", birthday: 1949, occupation: "singer"}
+    
+    apply는 함수 매개변수를 처리하는 방법을 제외하면 call과 완전히 같다.
+    call은 일반적인 함수와 마찬가지로 매개변수를 직접 받지만 apply는 매개변수로 배열을 받는다
+    apply는 배열의 최솟값과 최대 값을 구할때 사용한다.
+    const array = [2,3, -5, 15 ,7];
+    Math.min.apply(null, array);
+    Math.max.apply(null, array);
+    Math.min(...array);
+
+    bind 
+    함수의 this 값을 영구적으로 바꿀수 있다.
+    const updateBruce = update.bind(bruece);
+
+    스코프
+    function f(x){      x의 스코프가 함수 f라고 말한다.
+        return x+3;
+    }
+    가시성이라고 불리는 스코프는 프로그램의 현재 실행 중인 부분, 즉 실행 컨텍스트에서 현재 보이고 접근할수 있는 식별자 들을 말한다.
+    존재한다는 말은 그 식별자가 메모리에 할당된 무언가를 가르키고 있다는 뜻이다. 존재하지만 스코프 안에 없는 변수
+    무언가가 더는 존재하지 않는다고 해서 자바스크립트는 메모리를 바로 회수하지 않는다 카비지 콜렉션 프로세스에 의해 메모리를 회수한다.
+
+    자바스크립트의 스코프는 정적이다.
+    정적 스코프는 어떤 변수가 함수 스코프 안에 있는지 함수를 정의할때 알수 있다는 뜻입니다.
+    
 
     객체
     const school = {
